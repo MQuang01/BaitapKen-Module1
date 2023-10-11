9d9a51114358d8410694d97533e8c5f076b5d4d8
Minh Quang <hcmquang2111@gmail.com>
2023-10-11 16:06:46 +0700

    Su dung toan tu

---
+++ sdung-toantu/app.js
@@ -0,0 +1,10 @@
+let inputWidth;
+let inputHeight;
+
+inputWidth = prompt("Nhap chieu rong : ");
+inputHeight = prompt("Nhap chieu dai : ");
+
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("Dien tich hinh chu nhat la : " + area)
